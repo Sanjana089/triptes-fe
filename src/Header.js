1f6884af12815b3fa0561bbfb33dc778c2bdd7a0
sanjana <sanjana@publicissapient.com>
2023-05-11 15:01:56 +0530

    responsive header

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,32 @@
-import * as React from "react";
 import "./Header.css";
+import React, { useState } from "react";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
+
   return (
     <div className="App-header">
       <img src="./logo3.png" className="App-logo" alt="logo" />
-      <a className="header-link" href="/">
-        Home
-      </a>
-      <a className="header-link" href="/about">
-        About
-      </a>
-      <a className="header-link" href="/contactus">
-        Contact Us
-      </a>
-      {/* <a className="header-link right" href="/">
-        My Account
-      </a> */}
+      <div className={`header-links ${isMenuOpen ? "open" : ""}`}>
+        <div className="dropdown-menu" onClick={toggleMenu}>
+          <span className="dropdown-icon">
+            {isMenuOpen ? <span>Menu &#8744;</span> : <span>Menu &#62;</span>}
+          </span>
+        </div>
+        <div className="dropdown-content">
+          <a className="header-link" href="/">
+            Home
+          </a>
+          <a className="header-link" href="/about">
+            About
+          </a>
+          <a className="header-link" href="/contactus">
+            Contact Us
+          </a>
+        </div>
+      </div>
       <button className="sign-in-button right" type="submit">
         Sign In
       </button>
