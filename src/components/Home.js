c96440d89a318fa98564bf0de851c531cc59896f
sanjana <sanjana@publicissapient.com>
2023-05-11 23:13:40 +0530

    added Ongoing Tour Card

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,25 @@
 import Header from "../Header";
 import "./Home.css";
+import Tours from "../data/Tours.json"
+import TourCard from "./TourCard";
 
 const Home = () => {
-  console.log("Here");
   return (
     <div className="Home">
       <Header></Header>
       <br />
-      <h1>Trending</h1>
-      <h3>Coming Soon</h3>
+      <h1>Ongoing</h1>
+      {Tours.map((tour, i) => (
+        <TourCard
+          key={i}
+          id={i}
+          title={tour.title}
+          subtitle={tour.subtitle}
+          price={tour.price}
+          rating={tour.rating}
+          image={tour.image}
+        />
+      ))}
     </div>
   );
 };
