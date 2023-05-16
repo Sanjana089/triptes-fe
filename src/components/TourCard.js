b91fb9671a52e01d85843c24fd7626bec9dd6ed2
sanjana <sanjana@publicissapient.com>
2023-05-16 10:57:44 +0530

    fixed card flex

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -4,13 +4,13 @@ import RatingStars from './RatingStars';
 
 export default function TourCard({ title, subtitle, price, rating, image }) {
   return (
-    <div className="card">
+    <div className="tour-card">
       <div className="image-container">
         <img src={image} alt="Tour Package" className="tour-image" />
       </div>
       <div className="card-content">
-        <h1 className="title">{title}</h1>
-        <h3 className="subtitle">{subtitle}</h3>
+        <h1 className="tour-card-title">{title}</h1>
+        <h3 className="tour-card-subtitle">{subtitle}</h3>
         <div className="price-rating">
           <span className="price">Price per person<br/><h1>₹{price}</h1></span>
           <span className="rating"><RatingStars rating={rating}/> <br/> Rating: {rating}</span>
