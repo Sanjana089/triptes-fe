b4eb06e58913902e2ad2d75bdb5c8bcd9409106f
sanjana <sanjana@publicissapient.com>
2023-05-17 20:13:03 +0530

    Fixed carousel heights and dots

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useLayoutEffect } from "react";
 import "./Carousel.css";
 
 const Carousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const slidesContainerRef = useRef(null);
+  const carouselRef = useRef(null);
 
-  const images = ["./kedarnath.jpg", "./dwarka.jpg"]; // Array of image URLs
+  const images = ["./2.png", "./3.jpg"]; // Array of image URLs
 
   useEffect(() => {
     // Auto-advance the carousel every 4 seconds
@@ -19,28 +21,46 @@ const Carousel = () => {
     setActiveIndex(index);
   };
 
+  const handleResize = () => {
+    if (carouselRef.current && slidesContainerRef.current) {
+      const currentSlideHeight =
+        slidesContainerRef.current.children[activeIndex].clientHeight;
+      carouselRef.current.style.height = `${currentSlideHeight}px`;
+    }
+  };
+
+  useLayoutEffect(() => {
+    // Update the height when the component mounts
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => window.removeEventListener("resize", handleResize);
+  });
   return (
-    <div className="carousel">
-      <div className="slides-container">
-      {images.map((image, index) => (
-        <img
-          key={index}
-          src={image}
-          alt={`Slide ${index + 1}`}
-          className={index === activeIndex ? "slide active" : "slide"}
-        />
-      ))}
+    <div className="carousel" ref={carouselRef}>
+      <div className="slides-container" ref={slidesContainerRef}>
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Slide ${index + 1}`}
+            className={index === activeIndex ? "slide active" : "slide"}
+          />
+        ))}
+      </div>
+      <div className="dots-container">
+        {images.map((_, index) => (
+          <span
+            key={index}
+            className={index === activeIndex ? "dot active" : "dot"}
+            onClick={() => handleDotClick(index)}
+          ></span>
+        ))}
+      </div>
     </div>
-    <div className="dots-container">
-    {images.map((_, index) => (
-      <span
-        key={index}
-        className={index === activeIndex ? 'dot active' : 'dot'}
-        onClick={() => handleDotClick(index)}
-      ></span>
-    ))}
-  </div>
-  </div>
   );
 };
 
