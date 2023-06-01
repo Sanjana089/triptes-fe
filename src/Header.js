3837dfec1f16349567e30eaacb6821cf4dfdeaa3
sanjana <sanjana@publicissapient.com>
2023-06-01 17:11:04 +0530

    shifted menu from right to left in mobile view

--- src/Header.js
+++ src/Header.js
@@ -8,32 +8,39 @@ export default function Header() {
 
   return (
     <div className="App-header">
-      <a href="/">
-    <img src="./logo3.png" className="App-logo" alt="logo" />
-  </a>
+      <a href="/" className="App-logo-link">
+        <img src="./logo3.png" className="App-logo" alt="logo" />
+      </a>
       <div className={`header-links ${isMenuOpen ? "open" : ""}`}>
         <div className="dropdown-menu" onClick={toggleMenu}>
           <span className={`dropdown-icon ${isMenuOpen ? "open" : ""}`}>
             <span></span>
             <span></span>
             <span></span>
-            {/* {isMenuOpen ? <span></span> : <span>Menu &#62;</span>} */}
           </span>
         </div>
-        <div className="dropdown-content">
-          <a className="header-link" href="/">
-            Home
-          </a>
-          <a className="header-link" href="/about">
-            About
-          </a>
-          <a className="header-link" href="/contactus">
-            Contact Us
-          </a>
-      <button className="sign-in-button right" type="submit">
-        Sign In
-      </button>
-      </div>
+        <div className={`left-column-menu ${isMenuOpen ? "open" : ""}`}>
+          <div className="close-button" onClick={toggleMenu}>
+            <span>&times;</span>
+          </div>
+          <div className="dropdown-content">
+            <a className="header-link" href="/">
+              Home
+            </a>
+            <a className="header-link" href="/about">
+              About
+            </a>
+            <a className="header-link" href="/contactus">
+              Contact Us
+            </a>
+          </div>
+        </div>
+        <a href="/" className="App-logo-link-mobile">
+          <img src="./logo3.png" className="App-logo" alt="logo" />
+        </a>
+        <button className="sign-in-button right" type="submit">
+          Sign In
+        </button>
       </div>
     </div>
   );
